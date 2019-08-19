0a05943e041c379265ff3253a5130a9aff1426c6
Clark Williams <theclarkwilliams@gmail.com>
2019-08-19 16:20:14 -0600

    MVP

--- users/users-model.js
+++ users/users-model.js
@@ -2,11 +2,16 @@ const db = require('../data/db-config.js');
 
 module.exports = {
     addUser,
+    findUser,
     getUsers
 }
 
 function addUser(user) {
     return db('users').insert(user);
+}
+
+function findUser(filter) {
+    return db('users').where(filter);
 }
 
 function getUsers() {
--- users/users-router.js
+++ users/users-router.js
@@ -2,14 +2,12 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 
 const Users = require('./users-model.js');
+const restricted = require('../auth/restricted-middleware.js');
 
 const router = express.Router();
 
 router.post('/register', (req, res) => {
     let user = req.body;
-    // const hash = bcrypt.hashSync(user.password);
-    // console.log(hash);
-    // user.password = hash;
     user.password = bcrypt.hashSync(user.password);
 
     if(user.username && user.password) {
@@ -25,7 +23,24 @@ router.post('/register', (req, res) => {
     }    
 })
 
-router.get('/users', (req, res) => {
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findUser({ username })
+    .first()
+    .then(user => {
+        if (!user || !bcrypt.compareSync(password, user.password)) {
+            return res.status(401).json({ error: 'Incorrect credentials' });
+        } else {
+            res.status(200).json({ message: `Welcome, ${user.username}!`})
+        }
+    })
+    .catch(err => {
+        res.status(500).json(err);
+    })
+})
+
+router.get('/users', restricted, (req, res) => {
     Users.getUsers()
         .then(list => {
             res.status(200).json(list);
